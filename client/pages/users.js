f6205d7103d6485df2f43cacb0190b36a4ddf11e
emiliano macias <emilianomacias0@gmail.com>
2016-09-14 12:53:13 -0500

    Se agregaron Users.html y users.js cuales permiten mostrar una tabla con los usuarios registrados

--- client/pages/users.js
+++ client/pages/users.js
@@ -3,3 +3,17 @@ Template.Users.onCreated(function(){
 		this.subscribe('allUsers');
 	});
 });
+
+Template.Users.helpers({
+	users: function(){
+		return Meteor.users.find();
+	},
+	userEmail: function(){
+		return this.emails[0].address;
+	},
+	isAdmin: function(){
+		return Roles.userIsInRole(this._id,'admin') ? 'admin' : 'normal';
+
+	}
+
+});
