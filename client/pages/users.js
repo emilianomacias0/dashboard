7266c977607c6cbfcc3b71c21440e5465d69df7c
emiliano macias <emilianomacias0@gmail.com>
2016-09-14 15:21:28 -0500

    fin del proyecto

--- client/pages/users.js
+++ client/pages/users.js
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 Template.Users.onCreated(function(){
 	this.autorun(()=>{
 		this.subscribe('allUsers');
@@ -12,8 +14,30 @@ Template.Users.helpers({
 		return this.emails[0].address;
 	},
 	isAdmin: function(){
-		return Roles.userIsInRole(this._id,'admin') ? 'admin' : 'normal';
+		return Roles.userIsInRole(this._id,'admin') ? 'admin':'';
 
+	},
+	dateFormat: function(){
+		return moment(this.createdAt).format('MMM D YYYY');
+	},
+	editMode:function(){
+		return Session.get('currentUser') ? 'edit-mode':'';
+	},
+	currentEdit:function(){
+		let user = Session.get('currentUser');
+		return user._id === this._id;
+	}	
+
+});
+
+Template.Users.events({
+	'click .user_id':function(){
+		Session.set('currentUser',this);
+	},
+	'click .toggle-admin':function(){
+	Meteor.call('toggleAdmin',this._id);
+	},
+	'click .close-edit-mode':function(){
+		Session.set('currentUser',null);
 	}
-
 });
